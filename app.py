2efe42783d176a030d65102cc11db0caee4b96c9
MrDangwal <128894838+MrDangwal@users.noreply.github.com>
2024-02-22 17:28:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
 from google_img_source_search import ReverseImageSearcher
-from PIL import Image
 import streamlit as st
-import requests
 import os
 import uuid
-from urllib.parse import quote
 
 uid = uuid.uuid4()
-
-def save_uploaded_file(uploaded_file):
-    if uploaded_file is not None:
-        # Create the "uploads" directory if it doesn't exist
-        if not os.path.exists("uploads"):
-            os.makedirs("uploads")
-        
-        with open(os.path.join("uploads", f"{uid}.png"), "wb") as f:
-            f.write(uploaded_file.getvalue())
-        return os.path.abspath(os.path.join("uploads", f"{uid}.png"))
-    return None
 
 def main():
     st.title("Reverse Image Search")
@@ -28,19 +14,30 @@ def main():
         tmp_path = save_uploaded_file(uploaded_image)
 
         if tmp_path is not None:
-            # Convert the local file path to a URL
-            local_url = f"http://localhost:8501/files/{quote(tmp_path)}"
+            # Create a temporary URL for the uploaded image
+            tmp_url = st.get_url(tmp_path)
+            if tmp_url is not None:
+                # Perform reverse image search
+                rev_img_searcher = ReverseImageSearcher()
+                try:
+                    res = rev_img_searcher.search(tmp_url)
+                    for search_item in res:
+                        st.write(f'Title: {search_item.page_title}')
+                        st.write(f'Site: {search_item.page_url}')
+                        st.write(f'Img: {search_item.image_url}\n')
+                except RuntimeError as e:
+                    st.error(f"An unexpected error occurred while processing the image: {e}")
 
-            # Perform reverse image search
-            rev_img_searcher = ReverseImageSearcher()
-            try:
-                res = rev_img_searcher.search(local_url)
-                for search_item in res:
-                    st.write(f'Title: {search_item.page_title}')
-                    st.write(f'Site: {search_item.page_url}')
-                    st.write(f'Img: {search_item.image_url}\n')
-            except RuntimeError as e:
-                st.error(f"An unexpected error occurred while processing the image: {e}")
+def save_uploaded_file(uploaded_file):
+    if uploaded_file is not None:
+        # Create the "uploads" directory if it doesn't exist
+        if not os.path.exists("uploads"):
+            os.makedirs("uploads")
+
+        with open(os.path.join("uploads", f"{uid}.png"), "wb") as f:
+            f.write(uploaded_file.getvalue())
+        return os.path.join("uploads", f"{uid}.png")
+    return None
 
 if __name__ == "__main__":
     main()
